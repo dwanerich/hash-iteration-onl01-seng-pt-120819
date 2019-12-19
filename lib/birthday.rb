fa6c1ad464dd22b9b63796ecba1bedc761746231
dwane richards <dwanerich@gmail.com>
2019-12-19 06:09:15 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -8,7 +8,5 @@ def happy_birthday(birthday_kids)
   birthday_kids.each do |name, age|
     age < 12 || age == 12
     puts "Happy Birthday #{name}! You are now #{age} years old!"
-    
-  end
   end
 end
